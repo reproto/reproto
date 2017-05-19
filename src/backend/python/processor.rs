5cd0d65dddb2476cc9fca1cbc6c9486d6f925a94
John-John Tedro <udoprog@tedro.se>
2017-05-19 20:43:41 +0200

    make python basic processor do more

--- src/backend/python/processor.rs
+++ src/backend/python/processor.rs
@@ -17,28 +17,7 @@ use errors::*;
 const INIT_PY: &str = "__init__.py";
 const EXT: &str = "py";
 
-pub trait Listeners {
-    fn class_added(&self,
-                   processor: &Processor,
-                   package: &ast::Package,
-                   fields: &Vec<Field>,
-                   class: &mut ClassSpec)
-                   -> Result<()>;
-
-    fn tuple_added(&self,
-                   processor: &Processor,
-                   package: &ast::Package,
-                   fields: &Vec<Field>,
-                   class: &mut ClassSpec)
-                   -> Result<()>;
-
-    fn interface_added(&self,
-                       processor: &Processor,
-                       package: &ast::Package,
-                       interface: &ast::InterfaceDecl,
-                       interface_spec: &mut ClassSpec)
-                       -> Result<()>;
-}
+pub trait Listeners {}
 
 #[derive(Clone)]
 pub struct Field {
@@ -64,6 +43,8 @@ pub struct Processor<'a> {
     env: &'a Environment,
     package_prefix: Option<ast::Package>,
     to_lower_snake: Box<naming::Naming>,
+    staticmethod: BuiltInName,
+    dict: BuiltInName,
 }
 
 const PYTHON_CONTEXT: &str = "python";
@@ -78,10 +59,167 @@ impl<'a> Processor<'a> {
             env: env,
             package_prefix: package_prefix,
             to_lower_snake: naming::SnakeCase::new().to_lower_snake(),
+            staticmethod: Name::built_in("staticmethod"),
+            dict: Name::built_in("dict"),
         }
     }
 
-    pub fn is_native(&self, ty: &ast::Type) -> bool {
+    /// Build a function that raises an exception if the given value `stmt` is None.
+    fn raise_if_none(&self, stmt: &Statement, field: &Field) -> Elements {
+        let mut raise_if_none = Elements::new();
+        let required_error = Variable::String(format!("{}: is a required field", field.name));
+
+        raise_if_none.push(python_stmt!["if ", &stmt, " is None:"]);
+        raise_if_none.push_nested(python_stmt!["raise Exception(", required_error, ")"]);
+
+        raise_if_none
+    }
+
+    fn encode_method<F>(&self,
+                        package: &ast::Package,
+                        fields: &Vec<Field>,
+                        builder: &BuiltInName,
+                        field_set: F)
+                        -> Result<MethodSpec>
+        where F: Fn(&str, Statement) -> Statement
+    {
+        let mut encode = MethodSpec::new("encode");
+        encode.push_argument(python_stmt!["self"]);
+
+        let mut encode_body = Elements::new();
+
+        encode_body.push(python_stmt!["data = ", builder, "()"]);
+
+        for field in fields {
+            match field.modifier {
+                ast::Modifier::Optional => {
+                    let mut check_if_none = Elements::new();
+                    let stmt = python_stmt!["self.", &field.ident];
+
+                    check_if_none.push(python_stmt!["if ", &stmt, " is not None:"]);
+
+                    let stmt = self.encode(package, &field.ty, stmt.clone())?;
+                    let stmt = field_set(&field.name, stmt);
+
+                    check_if_none.push_nested(stmt);
+
+                    encode_body.push(check_if_none);
+                }
+                _ => {
+                    let stmt = python_stmt!["self.", &field.ident];
+
+                    // TODO: make configurable
+                    encode_body.push(self.raise_if_none(&stmt, field));
+
+                    let stmt = python_stmt!["self.", &field.name];
+                    let stmt = self.encode(package, &field.ty, stmt)?;
+                    let stmt = field_set(&field.name, stmt);
+
+                    encode_body.push(stmt);
+                }
+            }
+        }
+
+        encode_body.push(python_stmt!["return data"]);
+
+        encode.push(encode_body.join(ElementSpec::Spacing));
+        Ok(encode)
+    }
+
+    fn encode_tuple_method(&self,
+                           package: &ast::Package,
+                           fields: &Vec<Field>)
+                           -> Result<MethodSpec> {
+        let mut values = Statement::new();
+
+        let mut encode = MethodSpec::new("encode");
+        encode.push_argument(python_stmt!["self"]);
+
+        let mut encode_body = Elements::new();
+
+        for field in fields {
+            let stmt = python_stmt!["self.", &field.name];
+            encode_body.push(self.raise_if_none(&stmt, field));
+            values.push(self.encode(package, &field.ty, stmt)?);
+        }
+
+        encode_body.push(python_stmt!["return (", values.join(", "), ")"]);
+        encode.push(encode_body.join(ElementSpec::Spacing));
+        Ok(encode)
+    }
+
+    fn optional_check(&self, var_name: &str, index: &Variable, stmt: &Statement) -> ElementSpec {
+        let mut check = Elements::new();
+
+        let mut none_check = Elements::new();
+        none_check.push(python_stmt![var_name, " = data[", index, "]"]);
+
+        let mut none_check_if = Elements::new();
+
+        let assign_var = python_stmt![var_name, " = ", stmt];
+
+        none_check_if.push(python_stmt!["if ", var_name, " is not None:"]);
+        none_check_if.push_nested(assign_var);
+
+        none_check.push(none_check_if);
+
+        check.push(python_stmt!["if ", index, " in data:"]);
+        check.push_nested(none_check.join(ElementSpec::Spacing));
+
+        check.push(python_stmt!["else:"]);
+        check.push_nested(python_stmt![var_name, " = None"]);
+
+        check.as_element_spec()
+    }
+
+    fn decode_method<F>(&self,
+                        package: &ast::Package,
+                        fields: &Vec<Field>,
+                        class: &ClassSpec,
+                        variable_fn: F)
+                        -> Result<MethodSpec>
+        where F: Fn(usize, &str) -> Variable
+    {
+        let mut decode = MethodSpec::new("decode");
+        decode.push_decorator(&self.staticmethod);
+        decode.push_argument(python_stmt!["data"]);
+
+        let mut decode_body = Elements::new();
+
+        let mut arguments = Statement::new();
+
+        for (i, field) in fields.iter().enumerate() {
+            let var_name = format!("f_{}", field.ident);
+            let var = variable_fn(i, &field.name);
+
+            let stmt = match field.modifier {
+                ast::Modifier::Optional => {
+                    let var_stmt = python_stmt![&var_name];
+                    let var_stmt = self.decode(package, &field.ty, var_stmt)?;
+
+                    self.optional_check(&var_name, &var, &var_stmt)
+                }
+                _ => {
+                    let var_stmt = python_stmt!["data[", &var, "]"];
+                    let var_stmt = self.decode(package, &field.ty, var_stmt)?;
+
+                    python_stmt![&var_name, " = ", &var_stmt].as_element_spec()
+                }
+            };
+
+            decode_body.push(stmt);
+            arguments.push(var_name);
+        }
+
+        let arguments = arguments.join(", ");
+        decode_body.push(python_stmt!["return ", &class.name, "(", arguments, ")"]);
+
+        decode.push(decode_body.join(ElementSpec::Spacing));
+
+        Ok(decode)
+    }
+
+    fn is_native(&self, ty: &ast::Type) -> bool {
         match *ty {
             ast::Type::I32 | ast::Type::U32 => true,
             ast::Type::I64 | ast::Type::U64 => true,
@@ -116,11 +254,7 @@ impl<'a> Processor<'a> {
         Ok(Name::imported_alias(&package, &custom, used).as_name())
     }
 
-    pub fn encode(&self,
-                  package: &ast::Package,
-                  ty: &ast::Type,
-                  stmt: Statement)
-                  -> Result<Statement> {
+    fn encode(&self, package: &ast::Package, ty: &ast::Type, stmt: Statement) -> Result<Statement> {
         // TODO: do not skip conversion if strict type checking is enabled
         if self.is_native(ty) {
             return Ok(stmt);
@@ -144,11 +278,7 @@ impl<'a> Processor<'a> {
         Ok(stmt)
     }
 
-    pub fn decode(&self,
-                  package: &ast::Package,
-                  ty: &ast::Type,
-                  stmt: Statement)
-                  -> Result<Statement> {
+    fn decode(&self, package: &ast::Package, ty: &ast::Type, stmt: Statement) -> Result<Statement> {
         // TODO: do not skip conversion if strict type checking is enabled
         if self.is_native(ty) {
             return Ok(stmt);
@@ -204,7 +334,7 @@ impl<'a> Processor<'a> {
     fn process_type<L>(&self,
                        package: &ast::Package,
                        ty: &ast::TypeDecl,
-                       listeners: &L)
+                       _listeners: &L)
                        -> Result<ClassSpec>
         where L: Listeners
     {
@@ -235,7 +365,7 @@ impl<'a> Processor<'a> {
                     }
                 }
 
-                listeners.tuple_added(self, package, &fields, &mut class)?;
+                self.tuple_added(package, &fields, &mut class)?;
                 Ok(class)
             }
             _ => Err(format!("unsupported type: {:?}", ty).into()),
@@ -260,7 +390,7 @@ impl<'a> Processor<'a> {
     fn process_message<L>(&self,
                           package: &ast::Package,
                           message: &ast::MessageDecl,
-                          listeners: &L)
+                          _listeners: &L)
                           -> Result<ClassSpec>
         where L: Listeners
     {
@@ -300,14 +430,26 @@ impl<'a> Processor<'a> {
             }
         }
 
-        listeners.class_added(self, package, &fields, &mut class)?;
+        let decode = self.decode_method(package,
+                           &fields,
+                           &class,
+                           |_, name| Variable::String(name.to_owned()))?;
+
+        class.push(decode);
+
+        let encode = self.encode_method(package, &fields, &self.dict, |name, stmt| {
+                python_stmt!["data[", Variable::String(name.to_owned()), "] = ", stmt]
+            })?;
+
+        class.push(encode);
+
         Ok(class)
     }
 
     fn process_interface<L>(&self,
                             package: &ast::Package,
                             interface: &ast::InterfaceDecl,
-                            listeners: &L)
+                            _listeners: &L)
                             -> Result<Vec<ClassSpec>>
         where L: Listeners
     {
@@ -315,7 +457,7 @@ impl<'a> Processor<'a> {
 
         let mut interface_spec = ClassSpec::new(&interface.name);
 
-        listeners.interface_added(self, package, interface, &mut interface_spec)?;
+        interface_spec.push(self.interface_decode_method(interface)?);
 
         let mut interface_fields: Vec<Field> = Vec::new();
 
@@ -381,7 +523,19 @@ impl<'a> Processor<'a> {
                 }
             }
 
-            listeners.class_added(self, package, &fields, &mut class)?;
+            let decode = self.decode_method(package,
+                               &fields,
+                               &class,
+                               |_, name| Variable::String(name.to_owned()))?;
+
+            class.push(decode);
+
+            let encode = self.encode_method(package, &fields, &self.dict, |name, stmt| {
+                    python_stmt!["data[", Variable::String(name.to_owned()), "] = ", stmt]
+                })?;
+
+            class.push(encode);
+
             classes.push(class);
         }
 
@@ -476,6 +630,55 @@ impl<'a> Processor<'a> {
         }
 
         Ok(())
+    }
+
+    fn tuple_added(&self,
+                   package: &ast::Package,
+                   fields: &Vec<Field>,
+                   class: &mut ClassSpec)
+                   -> Result<()> {
+
+        let decode = self.decode_method(package,
+                           fields,
+                           class,
+                           |i, _| Variable::Literal(i.to_string()))?;
+
+        let encode = self.encode_tuple_method(package, fields)?;
+
+        class.push(decode);
+        class.push(encode);
+        Ok(())
+    }
+
+    fn interface_decode_method(&self, interface: &ast::InterfaceDecl) -> Result<MethodSpec> {
+        let mut decode = MethodSpec::new("decode");
+        decode.push_decorator(&self.staticmethod);
+        decode.push_argument(python_stmt!["data"]);
+
+        let mut decode_body = Elements::new();
+
+        decode_body.push(python_stmt!["type = data[", Variable::String("type".to_owned()), "]"]);
+
+        for (_, ref sub_type) in &interface.sub_types {
+            for name in sub_type.options.lookup_string("name") {
+                let type_name = Name::local(&sub_type.name).as_name();
+
+                let mut check = Elements::new();
+
+                check.push(python_stmt!["if type == ", Variable::String(name.to_owned()), ":"]);
+                check.push_nested(python_stmt!["return ", type_name, ".decode(data)"]);
+
+                decode_body.push(check);
+            }
+        }
+
+        decode_body.push(python_stmt!["raise Exception(",
+                                      Variable::String("bad type".to_owned()),
+                                      " + type)"]);
+
+        decode.push(decode_body.join(ElementSpec::Spacing));
+
+        Ok(decode)
     }
 
     pub fn process<L>(&self, listeners: &L) -> Result<()>
