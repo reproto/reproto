1b6b957ace683c423a432959980ab228a8d6968b
John-John Tedro <udoprog@tedro.se>
2017-06-17 00:02:01 +0200

    update projects

--- it/it.mk
+++ it/it.mk
@@ -1,3 +1,5 @@
+project_js = $(CURDIR)/../project-js
+
 EXPECTED = expected
 OUTPUT = output
 PROTO_PATH = proto
@@ -21,7 +23,11 @@ rust_args ?=
 all: clean it
 
 it: ${SUITES}
-	@echo "Verifying"
+	make diffcheck
+	make $(SUITES:%=%-input)
+
+diffcheck:
+	@echo "Verifying Diffs"
 	@diff -ur $(EXPECTED) $(OUTPUT)
 
 update: ${SUITES}
@@ -29,20 +35,31 @@ update: ${SUITES}
 	git add $(EXPECTED)
 
 clean:
+	rm -rf project-*
 	@${RM} -rf output
 
 python:
 	@echo "Building Python"
 	@${TOOL} compile -b python ${python_args} -o ${PYTHON_OUT} --path ${PROTO_PATH} --package ${TARGET}
 
+python-input:
+
 rust:
 	@echo "Building Rust"
 	@${TOOL} compile -b rust ${python_args} -o ${RUST_OUT} --path ${PROTO_PATH} --package ${TARGET}
+
+rust-input:
 
 js:
 	@echo "Building JavaScript"
 	@${TOOL} compile -b js ${js_args} -o ${JS_OUT} --path ${PROTO_PATH} --package ${TARGET}
 
+js-input:
+	@rsync -rav ${project_js}/ ./project-js/
+	cd ./js-project && make
+
 java:
 	@echo "Building Java"
 	@${TOOL} compile -b java ${java_args} -o ${JAVA_OUT} --path ${PROTO_PATH} --package ${TARGET}
+
+java-input:
---
+++ it/project-js/src/script.js
@@ -0,0 +1,14 @@
+import {Test} from './test.js';
+import * as readline from 'readline';
+
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false
+});
+
+rl.on('line', function(line) {
+  let data = JSON.parse(line);
+  console.log(data);
+  Test.decode(data);
+})
