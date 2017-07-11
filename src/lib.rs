3d3ae146ff9eedb81a3c9e2f90c372e115104268
John-John Tedro <udoprog@tedro.se>
2017-07-11 16:51:06 +0200

    move error handling

--- src/lib.rs
+++ src/lib.rs
@@ -7,15 +7,10 @@ extern crate error_chain;
 #[macro_use]
 extern crate log;
 
-extern crate toml;
-extern crate url;
-extern crate url_serde;
+extern crate ansi_term;
 extern crate clap;
 extern crate linked_hash_map;
 extern crate pulldown_cmark;
-extern crate reproto_core;
-extern crate reproto_parser;
-extern crate reproto_repository;
 extern crate reproto_backend;
 extern crate reproto_backend_doc;
 extern crate reproto_backend_java;
@@ -23,11 +18,18 @@ extern crate reproto_backend_js;
 extern crate reproto_backend_json;
 extern crate reproto_backend_python;
 extern crate reproto_backend_rust;
+extern crate reproto_core;
+extern crate reproto_parser;
+extern crate reproto_repository;
+extern crate toml;
+extern crate url;
+extern crate url_serde;
 
 pub mod ops;
 pub mod config;
 pub mod errors;
 pub mod logger;
+pub mod error_handling;
 
 pub(crate) use clap::{App, Arg, ArgMatches, SubCommand};
 pub(crate) use config::read_config;
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,14 @@
-#[macro_use]
 extern crate log;
 extern crate reproto_core;
 extern crate reproto_parser;
 extern crate reproto_backend;
 extern crate clap;
 extern crate reproto;
-extern crate ansi_term;
 
+use reproto::error_handling;
 use reproto::errors::*;
 use reproto::logger;
 use reproto::ops;
-use reproto_backend as backend;
-use reproto_core as core;
-use reproto_parser as parser;
 
 const VERSION: &'static str = env!("CARGO_PKG_VERSION");
 
@@ -36,179 +32,6 @@ fn setup_logger(matches: &clap::ArgMatches) -> Result<()> {
     Ok(())
 }
 
-fn print_error<S: AsRef<str>>(m: S, p: &core::ErrorPos) -> Result<()> {
-    use std::iter::repeat;
-    use std::cmp::max;
-    use ansi_term::Colour::{Blue, Red};
-
-    let (line_str, line, (s, e)) = parser::find_line(&p.path, (p.start, p.end))?;
-
-    println!("{}:{}:{}-{}:", p.path.display(), line + 1, s + 1, e + 1);
-
-    let line_no = format!("{:>3}:", line + 1);
-
-    let mut indicator = String::new();
-
-    indicator.extend(repeat(' ').take(line_no.len() + s + 1));
-    indicator.extend(repeat('^').take(max(1, e - s)));
-
-    println!("{} {}", Blue.paint(line_no), line_str);
-    println!("{}{}{}",
-             Red.paint(indicator),
-             Red.paint(" - "),
-             Red.paint(m.as_ref()));
-
-    Ok(())
-}
-
-fn handle_core_error(e: &core::errors::ErrorKind) -> Result<bool> {
-    use core::errors::ErrorKind::*;
-
-    let out = match *e {
-        Pos(ref m, ref p) => {
-            print_error(m, p)?;
-            true
-        }
-        DeclMerge(ref m, ref source, ref target) => {
-            print_error(m, source)?;
-            print_error("previous declaration here", target)?;
-            true
-        }
-        FieldConflict(ref name, ref source, ref target) => {
-            print_error(&format!("conflict in field `{}`", name), source)?;
-            print_error("previous declaration here", target)?;
-            true
-        }
-        ExtendEnum(ref m, ref source, ref enum_target) => {
-            print_error(m, source)?;
-            print_error("previous declaration here", enum_target)?;
-            true
-        }
-        ReservedField(ref field_pos, ref reserved_pos) => {
-            print_error("field reserved", field_pos)?;
-            print_error("field reserved here", reserved_pos)?;
-            true
-        }
-        MatchConflict(ref source, ref target) => {
-            print_error("conflicts with existing clause", source)?;
-            print_error("existing clause here", target)?;
-            true
-        }
-        _ => false,
-    };
-
-    Ok(out)
-}
-
-fn handle_backend_error(e: &backend::errors::ErrorKind) -> Result<bool> {
-    use backend::errors::ErrorKind::*;
-
-    let out = match *e {
-        Pos(ref m, ref p) => {
-            print_error(m, p)?;
-            true
-        }
-        Core(ref e) => {
-            return handle_core_error(e);
-        }
-        Parser(ref e) => {
-            return handle_parser_error(e);
-        }
-        MissingRequired(ref names, ref location, ref fields) => {
-            print_error(&format!("missing required fields: {}", names.join(", ")),
-                        location)?;
-
-            for f in fields {
-                print_error("required field", f)?;
-            }
-
-            true
-        }
-        _ => false,
-    };
-
-    Ok(out)
-}
-
-fn handle_parser_error(e: &parser::errors::ErrorKind) -> Result<bool> {
-    use parser::errors::ErrorKind::*;
-    use ansi_term::Colour::Red;
-
-    let out = match *e {
-        Pos(ref m, ref p) => {
-            print_error(m, p)?;
-            true
-        }
-        Core(ref e) => {
-            return handle_core_error(e);
-        }
-        Syntax(ref p, ref expected) => {
-            let m = if !expected.is_empty() {
-                format!("unexpected token, expected one of: {}", expected.join(", "))
-            } else {
-                String::from("syntax error")
-            };
-
-            if let Some(ref pos) = *p {
-                print_error(m, pos)?;
-            } else {
-                println!("{}", Red.paint(m));
-            }
-
-            true
-        }
-        Parse(ref message, ref pos) => {
-            print_error(message, pos)?;
-            true
-        }
-        FieldConflict(ref name, ref source, ref target) => {
-            print_error(&format!("conflict in field `{}`", name), source)?;
-            print_error("previous declaration here", target)?;
-            true
-        }
-        EnumVariantConflict(ref pos, ref other) => {
-            print_error("conflicting name", pos)?;
-            print_error("previous name here", other)?;
-            true
-        }
-        _ => false,
-    };
-
-    Ok(out)
-}
-
-fn handle_error(e: &Error) -> Result<bool> {
-    use reproto::errors::ErrorKind::*;
-
-    let out = match *e.kind() {
-        Pos(ref m, ref p) => {
-            print_error(m, p)?;
-            true
-        }
-        Errors(ref errors) => {
-            for e in errors {
-                if !handle_error(e)? {
-                    print_root_error(e);
-                }
-            }
-
-            true
-        }
-        Core(ref core) => {
-            return handle_core_error(core);
-        }
-        Parser(ref e) => {
-            return handle_parser_error(e);
-        }
-        Backend(ref e) => {
-            return handle_backend_error(e);
-        }
-        _ => false,
-    };
-
-    Ok(out)
-}
-
 fn entry() -> Result<()> {
     let opts = setup_opts();
     let opts = ops::options(opts);
@@ -218,21 +41,9 @@ fn entry() -> Result<()> {
     Ok(())
 }
 
-fn print_root_error(e: &Error) {
-    error!("error: {}", e);
-
-    for cause in e.iter().skip(1) {
-        error!("  caused by: {}", cause);
-    }
-
-    if let Some(backtrace) = e.backtrace() {
-        error!("backtrace: {:?}", backtrace);
-    }
-}
-
 fn compiler_entry() -> Result<()> {
     if let Err(e) = entry() {
-        if !handle_error(&e)? {
+        if !error_handling::handle_error(&e)? {
             return Err(e);
         }
 
@@ -244,7 +55,7 @@ fn compiler_entry() -> Result<()> {
 
 fn main() {
     if let Err(e) = compiler_entry() {
-        print_root_error(&e);
+        error_handling::print_root_error(&e);
         ::std::process::exit(1);
     }
 
