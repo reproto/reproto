244829e1e972c85d85b3220e5bf7b3a834f536a0
John-John Tedro <udoprog@tedro.se>
2018-02-26 18:11:33 +0100

    eval: fix fonts path

--- eval/webpack.config.js
+++ eval/webpack.config.js
@@ -46,7 +46,7 @@ module.exports = {
           options: {
             name: '[name].[ext]',
             outputPath: 'fonts/',    // where the fonts will go
-            publicPath: '../'       // override the default path
+            publicPath: '/fonts/'       // override the default path
           }
         }]
       }
