6a7f4d5f8e8a85032bba582a544f06d170e36c10
John-John Tedro <udoprog@tedro.se>
2018-04-01 19:40:37 +0200

    eval: more syntax highligting for .reproto

--- eval/local_modules/brace/mode/reproto.js
+++ eval/local_modules/brace/mode/reproto.js
@@ -7,34 +7,39 @@ var TextHighlightRules = acequire("./text_highlight_rules").TextHighlightRules;
 var stringEscape = /\\(?:[nrt0'"\\]|x[\da-fA-F]{2}|u\{[\da-fA-F]{6}\})/.source;
 
 var reprotoHighlightRules = function() {
-    this.$rules = { start:
-       [ { token: 'variable.other.source.reproto',
-           regex: '\\b[A-Z][a-zA-Z0-9_]*\\b' },
-         { token: 'string.quoted.double.source.reproto',
-           regex: '"',
-           push:
-            [ { token: 'string.quoted.double.source.reproto',
-                regex: '"',
-                next: 'pop' },
-              { token: 'constant.character.escape.source.reproto',
-                regex: stringEscape },
-              { defaultToken: 'string.quoted.double.source.reproto' } ] },
-         { token: 'keyword.source.reproto',
-           regex: '\\b(?:type|interface|enum|tuple|service|use|as)\\b' },
-         { token: 'comment.line.doc.source.reproto',
-           regex: '//!.*$' },
-         { token: 'comment.line.double-dash.source.reproto',
-           regex: '//.*$' },
-         { token: 'storage.type.source.reproto',
-           regex: '\\b(any|float|double|boolean|string|bytes|datetime|u32|u64|i32|i64)\\b' },
-         { token : "paren.lparen", regex : /[\[({]/ },
-         { token : "paren.rparen", regex : /[\])}]/ },
-         { token: 'constant.language.source.reproto',
-           regex: '\\b(true|false)\\b' },
-         { token: 'constant.numeric.source.reproto',
-           regex: /\b([0-9][0-9_]*)(?:\.[0-9][0-9_]*)?(?:[Ee][+-][0-9][0-9_]*)?\b/ } ] };
+  this.$rules = { start: [
+    { token: 'variable.other.reproto',
+      regex: '\\b[A-Z][a-zA-Z0-9_]*\\b' },
+    { token: 'string.quoted.double.reproto',
+      regex: '"',
+      push: [
+        { token: 'string.quoted.double.reproto',
+          regex: '"',
+          next: 'pop' },
+        { token: 'constant.character.escape.reproto',
+          regex: stringEscape },
+        { defaultToken: 'string.quoted.double.reproto' }
+      ]
+    },
+    { token: 'keyword.reproto',
+      regex: '\\b(?:type|interface|enum|tuple|service|use|as)\\b' },
+    { token: 'comment.block.documentation.reproto',
+      regex: '(//!.*|///.*)$' },
+    { token: 'comment.line.double-slash.reproto',
+      regex: '//.*$' },
+    { token: 'storage.type.reproto',
+      regex: '\\b(any|float|double|boolean|string|bytes|datetime|u32|u64|i32|i64)\\b' },
+    { token: 'storage.modifier.reproto',
+      regex: '\\b(?:stream)\\b' },
+    { token : "paren.lparen", regex : /[\[({]/ },
+    { token : "paren.rparen", regex : /[\])}]/ },
+    { token: 'constant.language.reproto',
+      regex: '\\b(true|false)\\b' },
+    { token: 'constant.numeric.reproto',
+      regex: /\b([0-9][0-9_]*)(?:\.[0-9][0-9_]*)?(?:[Ee][+-][0-9][0-9_]*)?\b/ }
+  ] };
 
-    this.normalizeRules();
+  this.normalizeRules();
 };
 
 reprotoHighlightRules.metaData = {
