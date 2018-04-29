39cb069e19132cd8d50ab6f0eeb1134eb2d27926
John-John Tedro <udoprog@tedro.se>
2018-04-29 18:19:36 +0200

    break up languageserver module

---
+++ lib/languageserver/models.rs
@@ -0,0 +1,138 @@
+//! Data models that are shared for the language server.
+
+use core::{Position, RpVersionedPackage, Span};
+use std::collections::{BTreeSet, HashMap};
+use ty;
+use url::Url;
+
+/// Specifies a rename.
+#[derive(Debug, Clone)]
+pub enum Rename {
+    /// A prefix that should be name.
+    Prefix { prefix: String },
+    /// Rename a local type.
+    LocalType {
+        /// The path to the type.
+        path: Vec<String>,
+    },
+    /// A type that was requested to be renamed.
+    Type {
+        /// The prefix at which the type should be looked up from, indicating that it is in
+        /// a separate package.
+        prefix: Option<String>,
+        /// The path to the type.
+        path: Vec<String>,
+    },
+}
+
+/// The result of a find_rename call.
+#[derive(Debug, Clone)]
+pub enum RenameResult<'a> {
+    /// All renames are in the same file as where the rename was requested.
+    Local { ranges: &'a Vec<Range> },
+    /// A package was renamed, and the range indicates the endl of the import that should be
+    /// replaced.
+    ImplicitPackage {
+        ranges: &'a Vec<Range>,
+        position: Position,
+    },
+    /// Multiple different URLs.
+    Collections {
+        ranges: Option<&'a HashMap<Url, Vec<Range>>>,
+    },
+    /// Not supported, only used during development.
+    #[allow(unused)]
+    NotSupported,
+}
+
+/// Specifies a type completion.
+#[derive(Debug, Clone)]
+pub enum Completion {
+    /// Completions for type from a different package.
+    Absolute {
+        prefix: Option<String>,
+        path: Vec<String>,
+        suffix: Option<String>,
+    },
+    /// Completions for a given package.
+    Package { results: BTreeSet<String> },
+    /// Any type, including primitive types.
+    Any { suffix: Option<String> },
+}
+
+/// Specifies a jump
+#[derive(Debug, Clone)]
+pub enum Jump {
+    /// Perform an absolute jump.
+    Absolute {
+        prefix: Option<String>,
+        path: Vec<String>,
+    },
+    /// Jump to the specified package prefix.
+    Package { prefix: String },
+    /// Jump to where the prefix is declared.
+    Prefix { prefix: String },
+}
+
+/// Specifies a reference to some type.
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct Reference {
+    pub package: RpVersionedPackage,
+    pub path: Vec<String>,
+}
+
+/// The range of something.
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct Range {
+    /// Start position.
+    pub start: Position,
+    /// End position.
+    pub end: Position,
+}
+
+impl Range {
+    pub fn contains(&self, p: &Position) -> bool {
+        self.start <= *p && *p <= self.end
+    }
+}
+
+/// Information about a single prefix.
+#[derive(Debug, Clone)]
+pub struct Prefix {
+    /// The span of the prefix.
+    pub span: Span,
+    /// The package the prefix refers to.
+    pub package: RpVersionedPackage,
+    /// Is this package read-only?
+    pub read_only: bool,
+}
+
+/// Information about a single symbol.
+#[derive(Debug, Clone)]
+pub struct Symbol {
+    /// Url where the symbol is located.
+    pub url: Url,
+    /// Range where the symbol is located.
+    pub range: Range,
+    /// The name of the symbol.
+    pub name: String,
+    /// Markdown documentation comment.
+    pub comment: Option<String>,
+}
+
+impl Symbol {
+    /// Convert symbol into documentation.
+    pub fn to_documentation(&self) -> Option<ty::Documentation> {
+        let comment = match self.comment.as_ref() {
+            Some(comment) => comment,
+            None => return None,
+        };
+
+        let doc = ty::MarkupContent {
+            kind: ty::MarkupKind::Markdown,
+            value: comment.to_string(),
+        };
+
+        Some(ty::Documentation::MarkupContent(doc))
+    }
+}
