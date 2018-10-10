6c52b834560944fc19c923bf05a7aef83fe71523
John-John Tedro <udoprog@tedro.se>
2018-10-10 11:38:02 +0200

    Dart: Add keywords and clean up unused parts

--- lib/backend-dart/src/compiler.rs
+++ lib/backend-dart/src/compiler.rs
@@ -2,91 +2,26 @@
 
 use backend::PackageProcessor;
 use core::errors::*;
-use core::{self, Handle, Loc, RelativePath, RelativePathBuf};
-use flavored::{
-    RpEnumBody, RpField, RpInterfaceBody, RpName, RpPackage, RpServiceBody, RpTupleBody,
-    RpTypeBody, RpVariant, DartFlavor,
-};
-use genco::dart;
-use genco::{Cons, IntoTokens, Quoted, Dart, Tokens};
+use core::{self, Handle, Loc};
 use dart_file_spec::DartFileSpec;
-use std::collections::{BTreeMap, BTreeSet};
-use std::fmt;
+use flavored::{
+    DartFlavor, RpEnumBody, RpField, RpInterfaceBody, RpName, RpServiceBody,
+    RpTupleBody, RpTypeBody,
+};
+use genco::{Dart, IntoTokens, Quoted, Tokens};
 use std::rc::Rc;
 use trans::{self, Translated};
 use utils::Comments;
-use {Options, Service, EXT, LIB, MOD, TYPE_SEP};
-
-/// #[allow(non_camel_case_types)] attribute.
-pub struct AllowNonCamelCaseTypes;
-
-impl<'a> IntoTokens<'a, Dart<'a>> for AllowNonCamelCaseTypes {
-    fn into_tokens(self) -> Tokens<'a, Dart<'a>> {
-        "#[allow(non_camel_case_types)]".into()
-    }
-}
-
-/// Serializer derives.
-pub struct Derives;
-
-impl<'a> IntoTokens<'a, Dart<'a>> for Derives {
-    fn into_tokens(self) -> Tokens<'a, Dart<'a>> {
-        "#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]".into()
-    }
-}
-
-pub struct EnumDerives;
-
-impl<'a> IntoTokens<'a, Dart<'a>> for EnumDerives {
-    fn into_tokens(self) -> Tokens<'a, Dart<'a>> {
-        "#[derive(Clone, Copy, Debug, PartialEq, Eq)]".into()
-    }
-}
-
-/// A serde rename annotation.
-pub struct Rename<'a>(&'a str);
-
-impl<'a> IntoTokens<'a, Dart<'a>> for Rename<'a> {
-    fn into_tokens(self) -> Tokens<'a, Dart<'a>> {
-        toks!["#[serde(rename = ", self.0.quoted(), ")]"]
-    }
-}
-
-/// Tag attribute.
-pub struct Tag<'a>(&'a str);
-
-impl<'a> IntoTokens<'a, Dart<'a>> for Tag<'a> {
-    fn into_tokens(self) -> Tokens<'a, Dart<'a>> {
-        toks!["#[serde(tag = ", self.0.quoted(), ")]"]
-    }
-}
-
-/// Untagged attribute.
-pub struct Untagged;
-
-impl<'el> IntoTokens<'el, Dart<'el>> for Untagged {
-    fn into_tokens(self) -> Tokens<'el, Dart<'el>> {
-        toks!["#[serde(untagged)]"]
-    }
-}
+use {EXT, TYPE_SEP};
 
 pub struct Compiler<'el> {
     pub env: &'el Translated<DartFlavor>,
-    options: Options,
     handle: &'el Handle,
 }
 
 impl<'el> Compiler<'el> {
-    pub fn new(
-        env: &'el Translated<DartFlavor>,
-        options: Options,
-        handle: &'el Handle,
-    ) -> Compiler<'el> {
-        Compiler {
-            env,
-            options,
-            handle,
-        }
+    pub fn new(env: &'el Translated<DartFlavor>, handle: &'el Handle) -> Compiler<'el> {
+        Compiler { env, handle }
     }
 
     /// Build an implementation of the given name and body.
@@ -101,19 +36,11 @@ impl<'el> Compiler<'el> {
     }
 
     /// Convert the type name
-    ///
-    /// Optionally also emit the necessary attributes to suppress warnings for bad naming
-    /// conventions.
-    fn convert_type_name(&self, name: &RpName) -> (Rc<String>, Tokens<'static, Dart<'static>>) {
-        let attributes = if name.path.len() > 1 {
-            AllowNonCamelCaseTypes.into_tokens()
-        } else {
-            Tokens::new()
-        };
-
-        (Rc::new(name.join(TYPE_SEP)), attributes)
+    fn convert_type_name(&self, name: &RpName) -> Rc<String> {
+        Rc::new(name.join(TYPE_SEP))
     }
 
+    /// Convert field into type.
     fn into_type<'a>(&self, field: &'a RpField) -> Result<Tokens<'a, Dart<'a>>> {
         let stmt = toks![field.ty.clone()];
 
@@ -146,102 +73,22 @@ impl<'el> Compiler<'el> {
     }
 
     // Build the corresponding element out of a field declaration.
-    fn field_element<'a>(&self, field: &'a RpField, is_pub: bool) -> Result<Tokens<'a, Dart<'a>>> {
+    fn field_element<'a>(&self, field: &'a RpField) -> Result<Tokens<'a, Dart<'a>>> {
         let mut t = Tokens::new();
 
         let ident = field.safe_ident();
         let type_spec = self.into_type(field)?;
 
-        if field.is_optional() {
-            t.push(toks!["#[serde(skip_serializing_if=\"Option::is_none\")]"]);
-        }
-
-        if field.name() != ident {
-            t.push(Rename(field.name()));
-        }
-
         t.push_into(|t| {
-            if is_pub {
-                t.append("pub ");
-            }
-
-            t.append(toks![ident, ": ", type_spec, ","]);
+            t.append(toks![type_spec, " ", ident, ";"]);
         });
 
         Ok(t.into())
     }
 
     pub fn compile(&self) -> Result<()> {
-        let mut files = self.populate_files()?;
-        self.write_mod_files(&files)?;
+        let files = self.populate_files()?;
         self.write_files(files)
-    }
-
-    fn write_mod_files(&self, files: &BTreeMap<RpPackage, DartFileSpec>) -> Result<()> {
-        let mut packages: BTreeMap<RelativePathBuf, BTreeSet<String>> = BTreeMap::new();
-        let mut root_names = BTreeSet::new();
-
-        for (package, _) in files {
-            let mut current = RelativePathBuf::new();
-
-            let mut it = package.parts().peekable();
-
-            if let Some(root) = it.peek() {
-                root_names.insert(root.to_string());
-            }
-
-            while let Some(part) = it.next() {
-                current = current.join(part);
-
-                if let Some(next) = it.peek() {
-                    let mut full_path = current.join(MOD);
-                    full_path.set_extension(self.ext());
-
-                    packages
-                        .entry(full_path)
-                        .or_insert_with(BTreeSet::new)
-                        .insert(next.to_string());
-                }
-            }
-        }
-
-        let mut root_mod = RelativePathBuf::new().join(MOD);
-        root_mod.set_extension(self.ext());
-        packages.insert(root_mod, root_names);
-
-        let handle = self.handle();
-
-        for (full_path, children) in packages {
-            // skip writing mod if lib file exists.
-            let lib_path = full_path.with_file_name(LIB).with_extension(self.ext());
-            let parent = full_path.parent().unwrap_or(RelativePath::new("."));
-
-            if !self.handle.is_dir(&parent) {
-                debug!("+dir: {}", parent.display());
-                handle.create_dir_all(&parent)?;
-            }
-
-            // do not create mod file if there is a lib.rs in the root.
-            if lib_path.parent().is_none() && handle.is_file(&lib_path) {
-                debug!(
-                    "+mod: {} (skip due to: {})",
-                    full_path.display(),
-                    lib_path.display()
-                );
-                continue;
-            }
-
-            if !handle.is_file(&full_path) {
-                debug!("+mod: {}", full_path.display());
-                let mut f = handle.create(&full_path)?;
-
-                for child in children {
-                    writeln!(f, "pub mod {};", child)?;
-                }
-            }
-        }
-
-        Ok(())
     }
 }
 
@@ -266,48 +113,30 @@ impl<'el> PackageProcessor<'el, DartFlavor, Loc<RpName>> for Compiler<'el> {
     }
 
     fn process_tuple(&self, out: &mut Self::Out, body: &'el RpTupleBody) -> Result<()> {
-        let mut fields = Tokens::new();
-
-        for field in &body.fields {
-            fields.append(toks!["pub ", self.into_type(field)?]);
-        }
-
-        let (name, attributes) = self.convert_type_name(&body.name);
+        let name = self.convert_type_name(&body.name);
 
         let mut t = Tokens::new();
 
         t.push_unless_empty(Comments(&body.comment));
-        t.push_unless_empty(attributes);
-        t.push(Derives);
-        t.push(toks!["pub struct ", name, "(", fields.join(", "), ");",]);
+        t.push(toks!["class ", name, "{", "}"]);
 
         out.0.push(t);
         Ok(())
     }
 
     fn process_enum(&self, out: &mut Self::Out, body: &'el RpEnumBody) -> Result<()> {
-        let (name, mut attributes) = self.convert_type_name(&body.name);
+        let name = self.convert_type_name(&body.name);
 
         // variant declarations
         let mut vars = Tokens::new();
         // body of value function
         let mut match_body = Tokens::new();
 
-        if let core::RpVariants::Number { .. } = body.variants {
-            attributes.push(EnumDerives);
-        } else {
-            attributes.push(Derives);
-        }
-
         for v in body.variants.iter() {
             vars.push_unless_empty(Comments(&v.comment));
 
             match v.value {
                 core::RpVariantValue::String(string) => {
-                    if string != v.ident() {
-                        vars.push(Rename(string));
-                    }
-
                     push!(vars, v.ident(), ",");
                     push!(match_body, v.ident(), " => ", string.quoted(), ",");
                 }
@@ -322,7 +151,6 @@ impl<'el> PackageProcessor<'el, DartFlavor, Loc<RpName>> for Compiler<'el> {
             let mut t = Tokens::new();
 
             t.push_unless_empty(Comments(&body.comment));
-            t.push_unless_empty(attributes);
             t.push(toks!["pub enum ", name.clone(), " {"]);
             t.nested(vars);
             t.push("}");
@@ -347,21 +175,15 @@ impl<'el> PackageProcessor<'el, DartFlavor, Loc<RpName>> for Compiler<'el> {
             t
         });
 
-        // Serialize impl for numerics.
-        if let core::RpVariants::Number { ref variants } = body.variants {
-        }
-
         return Ok(());
     }
 
     fn process_type(&self, out: &mut Self::Out, body: &'el RpTypeBody) -> Result<()> {
-        let (name, attributes) = self.convert_type_name(&body.name);
+        let name = self.convert_type_name(&body.name);
         let mut t = Tokens::new();
 
         t.push_unless_empty(Comments(&body.comment));
-        t.push_unless_empty(attributes);
-        t.push(Derives);
-        t.push(toks!["pub struct ", name.clone(), " {"]);
+        t.push(toks!["class ", name.clone(), " {"]);
 
         // fields
         t.nested({
@@ -371,7 +193,7 @@ impl<'el> PackageProcessor<'el, DartFlavor, Loc<RpName>> for Compiler<'el> {
                 t.push({
                     let mut t = Tokens::new();
                     t.push_unless_empty(Comments(&field.comment));
-                    t.push(self.field_element(field, true)?);
+                    t.push(self.field_element(field)?);
                     t
                 });
             }
@@ -394,20 +216,16 @@ impl<'el> PackageProcessor<'el, DartFlavor, Loc<RpName>> for Compiler<'el> {
     }
 
     fn process_interface(&self, out: &mut Self::Out, body: &'el RpInterfaceBody) -> Result<()> {
-        let (name, attributes) = self.convert_type_name(&body.name);
+        let name = self.convert_type_name(&body.name);
 
         let mut t = Tokens::new();
 
         t.push_unless_empty(Comments(&body.comment));
-        t.push_unless_empty(attributes);
-        t.push(Derives);
 
         match body.sub_type_strategy {
-            core::RpSubTypeStrategy::Tagged { ref tag, .. } => {
-                t.push(Tag(tag.as_str()));
+            core::RpSubTypeStrategy::Tagged { .. } => {
             }
             core::RpSubTypeStrategy::Untagged => {
-                t.push(Untagged);
             }
         }
 
@@ -419,13 +237,6 @@ impl<'el> PackageProcessor<'el, DartFlavor, Loc<RpName>> for Compiler<'el> {
 
                 t.push_unless_empty(Comments(&s.comment));
 
-                // TODO: clone should not be needed
-                if let Some(ref name) = s.sub_type_name {
-                    if name.as_str() != s.ident.as_str() {
-                        t.push(Rename(name));
-                    }
-                }
-
                 t.push(toks![s.ident.as_str(), " {"]);
 
                 t.push({
@@ -435,7 +246,7 @@ impl<'el> PackageProcessor<'el, DartFlavor, Loc<RpName>> for Compiler<'el> {
                         t.nested({
                             let mut t = Tokens::new();
                             t.push_unless_empty(Comments(&field.comment));
-                            t.push(self.field_element(field, false)?);
+                            t.push(self.field_element(field)?);
                             t
                         });
                     }
@@ -462,7 +273,7 @@ impl<'el> PackageProcessor<'el, DartFlavor, Loc<RpName>> for Compiler<'el> {
         Ok(())
     }
 
-    fn process_service(&self, out: &mut Self::Out, body: &'el RpServiceBody) -> Result<()> {
+    fn process_service(&self, _: &mut Self::Out, _: &'el RpServiceBody) -> Result<()> {
         Ok(())
     }
 }
--- lib/backend-dart/src/lib.rs
+++ lib/backend-dart/src/lib.rs
@@ -1,6 +1,5 @@
 #[macro_use]
 extern crate genco;
-#[macro_use]
 extern crate log;
 #[macro_use]
 extern crate reproto_backend as backend;
@@ -16,30 +15,21 @@ extern crate serde_derive;
 extern crate toml;
 
 mod compiler;
+mod dart_file_spec;
 mod flavored;
 mod module;
-mod dart_file_spec;
 mod utils;
 
-use backend::Initializer;
 use compiler::Compiler;
 use core::errors::*;
 use core::{CoreFlavor, Handle};
-use flavored::RpPackage;
-use genco::{Cons, Dart, Tokens};
 use manifest::{Lang, Manifest, NoModule, TryFromToml};
-use dart_file_spec::DartFileSpec;
 use std::any::Any;
-use std::collections::BTreeMap;
 use std::path::Path;
-use std::rc::Rc;
-use trans::{Packages, Session};
+use trans::Session;
 
-const LIB: &str = "lib";
-const MOD: &str = "mod";
-const EXT: &str = "rs";
+const EXT: &str = "dart";
 const TYPE_SEP: &'static str = "_";
-const SCOPE_SEP: &'static str = "::";
 
 #[derive(Clone, Copy, Default, Debug)]
 pub struct DartLang;
@@ -53,140 +43,89 @@ impl Lang for DartLang {
 
     fn keywords(&self) -> Vec<(&'static str, &'static str)> {
         vec![
-            ("as", "_as"),
-            ("break", "_break"),
-            ("const", "_const"),
-            ("continue", "_continue"),
-            ("crate", "_crate"),
-            ("else", "_else"),
-            ("enum", "_enum"),
-            ("extern", "_extern"),
-            ("false", "_false"),
-            ("fn", "_fn"),
-            ("for", "_for"),
-            ("if", "_if"),
-            ("impl", "_impl"),
-            ("in", "_in"),
-            ("let", "_let"),
-            ("loop", "_loop"),
-            ("match", "_match"),
-            ("mod", "_mod"),
-            ("move", "_move"),
-            ("mut", "_mut"),
-            ("pub", "_pub"),
-            ("ref", "_ref"),
-            ("return", "_return"),
-            ("self", "_self"),
-            ("static", "_static"),
-            ("struct", "_struct"),
-            ("super", "_super"),
-            ("trait", "_trait"),
-            ("true", "_true"),
-            ("type", "_type"),
-            ("unsafe", "_unsafe"),
-            ("use", "_use"),
-            ("where", "_where"),
-            ("while", "_while"),
-            ("abstract", "_abstract"),
-            ("alignof", "_alignof"),
-            ("become", "_become"),
-            ("box", "_box"),
-            ("do", "_do"),
-            ("final", "_final"),
-            ("macro", "_macro"),
-            ("offsetof", "_offsetof"),
-            ("override", "_override"),
-            ("priv", "_priv"),
-            ("proc", "_proc"),
-            ("pure", "_pure"),
-            ("sizeof", "_sizeof"),
-            ("typeof", "_typeof"),
-            ("unsized", "_unsized"),
-            ("virtual", "_virtual"),
-            ("yield", "_yield"),
+            ("abstract", "abstract_"),
+            ("dynamic", "dynamic_"),
+            ("implements", "implements_"),
+            ("show", "show_"),
+            ("as", "as_"),
+            ("else", "else_"),
+            ("import", "import_"),
+            ("static", "static_"),
+            ("assert", "assert_"),
+            ("enum", "enum_"),
+            ("in", "in_"),
+            ("super", "super_"),
+            ("async", "async_"),
+            ("export", "export_"),
+            ("interface", "interface_"),
+            ("switch", "switch_"),
+            ("await", "await_"),
+            ("external", "external_"),
+            ("is", "is_"),
+            ("sync", "sync_"),
+            ("break", "break_"),
+            ("extends", "extends_"),
+            ("library", "library_"),
+            ("this", "this_"),
+            ("case", "case_"),
+            ("factory", "factory_"),
+            ("mixin", "mixin_"),
+            ("throw", "throw_"),
+            ("catch", "catch_"),
+            ("false", "false_"),
+            ("new", "new_"),
+            ("true", "true_"),
+            ("class", "class_"),
+            ("final", "final_"),
+            ("null", "null_"),
+            ("try", "try_"),
+            ("const", "const_"),
+            ("finally", "finally_"),
+            ("on", "on_"),
+            ("typedef", "typedef_"),
+            ("continue", "continue_"),
+            ("for", "for_"),
+            ("operator", "operator_"),
+            ("var", "var_"),
+            ("covariant", "covariant_"),
+            ("Function", "Function_"),
+            ("part", "part_"),
+            ("void", "void_"),
+            ("default", "default_"),
+            ("get", "get_"),
+            ("rethrow", "rethrow_"),
+            ("while", "while_"),
+            ("deferred", "deferred_"),
+            ("hide", "hide_"),
+            ("return", "return_"),
+            ("with", "with_"),
+            ("do", "do_"),
+            ("if", "if_"),
+            ("set", "set_"),
+            ("yield", "yield_"),
         ]
     }
 }
 
 #[derive(Debug)]
-pub enum DartModule {
-}
+pub enum DartModule {}
 
 impl TryFromToml for DartModule {
     fn try_from_string(path: &Path, id: &str, value: String) -> Result<Self> {
-        use self::DartModule::*;
-
-        let result = match id {
-            _ => return NoModule::illegal(path, id, value),
-        };
-
-        Ok(result)
+        return NoModule::illegal(path, id, value);
     }
 
     fn try_from_value(path: &Path, id: &str, value: toml::Value) -> Result<Self> {
-        use self::DartModule::*;
-
-        let result = match id {
-            _ => return NoModule::illegal(path, id, value),
-        };
-
-        Ok(result)
+        return NoModule::illegal(path, id, value);
     }
-}
-
-pub struct Options {
-    pub packages: Rc<Packages>,
-}
-
-pub struct Root<'a, 'el: 'a> {
-    files: &'a mut BTreeMap<RpPackage, DartFileSpec<'el>>,
-}
-
-pub trait RootCodegen {
-    /// Generate root code.
-    fn generate(&self, root: Root) -> Result<()>;
-}
-
-pub struct Service<'a, 'el: 'a> {
-    body: &'el flavored::RpServiceBody,
-    container: &'a mut Tokens<'el, Dart<'el>>,
-    name: Cons<'el>,
-    attributes: &'a Tokens<'el, Dart<'el>>,
-}
-
-pub trait ServiceCodegen {
-    /// Generate service code.
-    fn generate(&self, service: Service) -> Result<()>;
-}
-
-fn options(modules: Vec<DartModule>, packages: Rc<Packages>) -> Result<Options> {
-    use self::DartModule::*;
-
-    let mut options = Options {
-        packages: packages,
-    };
-
-    for m in modules {
-        debug!("+module: {:?}", m);
-
-        let initializer: Box<Initializer<Options = Options>> = match m {
-        };
-
-        initializer.initialize(&mut options)?;
-    }
-
-    Ok(options)
 }
 
 fn compile(handle: &Handle, session: Session<CoreFlavor>, manifest: Manifest) -> Result<()> {
-    let modules = manifest::checked_modules(manifest.modules)?;
+    let _: Vec<DartModule> = manifest::checked_modules(manifest.modules)?;
     let packages = session.packages()?;
-    let options = options(modules, packages.clone())?;
 
-    let translator = session.translator(flavored::DartFlavorTranslator::new(
-        packages.clone(),
-    ))?;
+    let translator = session.translator(flavored::DartFlavorTranslator::new(packages.clone()))?;
     let session = session.translate(translator)?;
 
-    Compiler::new(&session, options, handle).compile()
+    Compiler::new(&session, handle).compile()
 }
