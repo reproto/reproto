adec05891ddb7547bbd8cffb04a01feda00f368e
John-John Tedro <udoprog@tedro.se>
2022-06-30 06:16:40 +0200

    Revert url to 2.1.0

--- examples/rust-example/src/main.rs
+++ examples/rust-example/src/main.rs
@@ -1,13 +1,14 @@
 use rust::gen::github::v3;
 
-fn main() -> Result<(), rust::gen::reproto::Error> {
+#[tokio::main]
+async fn main() -> Result<(), rust::gen::reproto::Error> {
     let client = reqwest::Client::new();
     let github = v3::Github_Reqwest::new(client, None)?;
 
-    let rate_limit = github.get_rate_limit()?;
+    let rate_limit = github.get_rate_limit().await?;
     println!("{:?}", rate_limit);
 
-    let gists = github.get_user_gists("udoprog".to_string())?;
+    let gists = github.get_user_gists("udoprog".to_string()).await?;
 
     for g in gists {
         println!("{:?}", g);
